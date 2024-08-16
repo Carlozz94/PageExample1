5052514c414d0a689074b73c51f1db54c7345b99
CarlosDev <cazc.zarate94@gmail.com>
2024-08-15 20:15:33 -0600

    Resueltos los conflictos al fusionar historias

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have the 'Paginaweb' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Paginaweb');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,36 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Paginaweb';
+  firstName: string = '';
+  lastName: string = '';
+  affair: string = '';
+  message: string = '';
+  phone: string = '';
+
+  handleSubmit(event: Event) {
+    event.preventDefault();
+
+    const mailToLink = document.querySelector('#mail') as HTMLAnchorElement;
+    mailToLink.setAttribute(
+      'href',
+      `mailto:cazc.zarate94@gmail.com?subject=asunto${this.affair}&body=nombre ${this.firstName}
+  apellidos ${this.lastName}
+  Mensaje ${this.message}
+  telefono ${this.phone} `
+    );
+    mailToLink.click();
+
+    this.clearForm();
+  }
+
+  clearForm() {
+    this.firstName = '';
+    this.lastName = '';
+    this.affair = '';
+    this.message = '';
+    this.phone = '';
+  }
 }
